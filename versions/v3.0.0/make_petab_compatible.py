388608396510f9eeccdbdedf054feb1442b00016
Paul <paul.lang@wolfson.ox.ac.uk>
2020-12-13 21:31:42 +0100

    make modelId petab compatible

--- versions/v3.0.0/make_petab_compatible.py
+++ versions/v3.0.0/make_petab_compatible.py
@@ -1,7 +1,10 @@
 import os
 from model_tools import make_petab_compatible
 
-in_file = os.path.join('results', 'cell_cycle_v3.0.0', '2020-11-13_22-20-23', 'cell_cycle_v3.0.0_sbml.xml')
-out_file = os.path.join('cell_cycle_v3.0.0_sbml.xml')
+# in_file = os.path.join('results', 'cell_cycle_v3.0.0', '2020-11-13_22-20-23', 'cell_cycle_v3.0.0_sbml.xml')
+# out_file = os.path.join('cell_cycle_v3.0.0_sbml.xml')
+
+in_file = os.path.join('results', 'cell_cycle_v3.0.0', '2020-11-16_13-28-44', 'cell_cycle_v3.0.0_sbml.xml')
+out_file = os.path.join('PEtab', 'cell_cycle_v3.0.0_sbml.xml')
 
 make_petab_compatible(in_file, out_file)
